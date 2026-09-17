eecced450829e98d33d5ab33705f47e174ff1231
Akanksha Batwal <akankshabatwal54@gmail.com>
2026-09-17 16:39:02 +0530

    Implement prime number check using method
    Implemented a Java program to check whether a given number is prime.
    Created an isPrime() method that checks divisibility using a for loop and returns a boolean result.
    The program takes user input using Scanner and displays whether the number is prime.

---
+++ PrimeorNot.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class PrimeorNot{
+    static boolean isPrime(int n){
+        if(n == 2){
+            return true;
+        }
+        for(int i = 2; i <= n -1; i++ ){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number:");
+        int num = sc.nextInt();
+        boolean result = isPrime(num);
+        System.out.println(result);
+    }
+}
