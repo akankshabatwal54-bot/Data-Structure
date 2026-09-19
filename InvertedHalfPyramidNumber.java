e58551fc04b8957b362e11cfcc2c81ab0acb53a9
Akanksha Batwal <akankshabatwal54@gmail.com>
2026-09-19 15:37:58 +0530

    Implement inverted half pyramid with numbers
    Implemented a Java program to print an inverted half-pyramid using numbers.
    Used nested for loops to reduce the number of elements in each row and create the inverted pattern.

---
+++ InvertedHalfPyramidNumber.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class InvertedHalfPyramidNumber{
+    public static void invertedPyramid(int n){
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n - i + 1; j++){
+                System.out.print(j + " ");
+
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number:");
+        int n = sc.nextInt();
+        invertedPyramid(n);
+    }
+}
