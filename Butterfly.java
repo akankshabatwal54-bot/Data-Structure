a1fef3295f201550a8193ed0719e77a35d984e93
Akanksha Batwal <akankshabatwal54@gmail.com>
2026-09-20 21:53:39 +0530

    Implement butterfly pattern using nested loops
    Implemented a Java program to print a butterfly pattern using nested for loops.
    Used stars and spaces to create the first and second halves of the pattern.

---
+++ Butterfly.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class Butterfly{
+    public static void butterfly(int n){
+    // first half
+    for(int i = 1; i <= n; i++){
+        // stars - *
+        for(int j = 1; j <= i; j++){
+            System.out.print("*");
+        }
+        // spaces - ""
+        for(int j = 1; j <= 2 * (n - i); j++){
+            System.out.print(" ");
+        }
+        // stars - *
+        for(int j = 1; j <= i; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    // second half
+    for(int i = n; i >= 1; i--){
+        // stars - *
+        for(int j = 1; j <= i; j++){
+            System.out.print("*");
+        }
+        // spaces - ""
+        for(int j = 1; j <= 2 * (n - i); j++){
+            System.out.print(" ");
+        }
+        // stars - *
+        for(int j = 1; j <= i; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n = sc.nextInt();
+        butterfly(n);
+    }
+}
