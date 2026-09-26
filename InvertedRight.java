93b4973effa1d2734965f19ef8f1db460e84684f
Akanksha Batwal <akankshabatwal54@gmail.com>
2026-09-26 12:27:01 +0530

    Implement inverted right aligned triangle pattern
    Implemented a Java program to print an inverted right-aligned triangle.
    Used nested for loops and an if-else condition to print underscores before the stars and create the required pattern.

---
+++ InvertedRight.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class InvertedRight{
+    public static void right(int n){
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if( j <= (n - i)){
+                    System.out.print("_");
+                } else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number:");
+        int n = sc.nextInt();
+        right(n);
+    }
+}
